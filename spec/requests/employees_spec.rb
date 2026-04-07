971554df4fd51c970fd8297baa24c03a063b4a19
Pavan Kumar Dega <pavankumardega24@gmail.com>
2026-04-07 05:08:33 +0000

    test: add employee creation test

--- spec/requests/employees_spec.rb
+++ spec/requests/employees_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Employees API", type: :request do
-  it "creates an employee" do
-    post "/employees", params: {
-      full_name: "Pavan",
-      job_title: "Engineer",
-      country: "India",
-      salary: 50000
-    }
+  describe "POST /employees" do
+    it "creates an employee" do
+      post "/employees", params: {
+        full_name: "Pavan",
+        job_title: "Engineer",
+        country: "India",
+        salary: 50000
+      }
 
-    expect(response).to have_http_status(201)
+      expect(response).to have_http_status(201)
+    end
   end
 end
