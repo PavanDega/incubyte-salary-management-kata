4fc1bd99d480bfce86c8fe47a98950f415491699
Pavan Kumar Dega <pavankumardega24@gmail.com>
2026-04-07 05:03:34 +0000

    test: create employee endpoint

---
+++ spec/requests/employees_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Employees API", type: :request do
+  it "creates an employee" do
+    post "/employees", params: {
+      full_name: "Pavan",
+      job_title: "Engineer",
+      country: "India",
+      salary: 50000
+    }
+
+    expect(response).to have_http_status(201)
+  end
+end
