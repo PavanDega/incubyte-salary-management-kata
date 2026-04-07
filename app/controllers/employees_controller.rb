b16ecf351d42f2c8fa3f5f3989b4978c70688761
Pavan Kumar Dega <pavankumardega24@gmail.com>
2026-04-07 05:24:55 +0000

    feat: implement full employee CRUD

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,13 +1,35 @@
 class EmployeesController < ApplicationController
+
+  def index
+    render json: Employee.all
+  end
+
+  def show
+    employee = Employee.find(params[:id])
+    render json: employee
+  end
+
   def create
     employee = Employee.create!(employee_params)
     render json: employee, status: :created
   end
 
-  
+  def update
+    employee = Employee.find(params[:id])
+    employee.update!(employee_params)
+    render json: employee
+  end
+
+  def destroy
+    employee = Employee.find(params[:id])
+    employee.destroy
+    head :no_content
+  end
+
   private
 
   def employee_params
     params.permit(:full_name, :job_title, :country, :salary)
   end
+
 end
